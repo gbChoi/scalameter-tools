c98901778b86bcb6d29fd9770f9334aa4ecd7cea
Gwangbae Choi <gwangbae.choi@epfl.ch>
2014-06-02 12:58:41 +0900

    fix path problem

--- scalameter-tools/src/scala/org/scalameter/tools/package.scala
+++
@@ -1,9 +0,0 @@
-package org.scalameter
-
-package object tools extends MeasureBuilder(Context.topLevel, Executor.Warmer.NoWarmer(), null, null) {
-  type Measurer = org.scalameter.Executor.Measurer
-  type MemoryFootprint = org.scalameter.Executor.Measurer.MemoryFootprint
-  type DefaultWarmer = org.scalameter.Executor.Warmer.Default
-  type NoWarmer = org.scalameter.Executor.Warmer.NoWarmer
-  val exec = org.scalameter.Key.exec
-}
