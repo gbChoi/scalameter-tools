9306976cf31e6d6fda7ba93563f0a7507d2a64fb
Gwangbae Choi <gwangbae.choi@epfl.ch>
2014-06-05 08:26:32 +0900

    fix some problem and handle errors in agent

--- scalameter-tools/src/main/resources/methodCallTracer.c
+++ scalameter-tools/src/main/resources/methodCallTracer.c
@@ -10,28 +10,36 @@ void JNICALL MethodEntry(jvmtiEnv *jvmti, JNIEnv* jni_env, jthread thread, jmeth
 	char *method_name, *class_signature;
 	jclass class;
 
-	(*jvmti)->RawMonitorEnter(jvmti, lock);
+	if ((*jvmti)->RawMonitorEnter(jvmti, lock) == JVMTI_ERROR_NONE) {
+		if ((*jvmti)->GetMethodName(jvmti, method, &method_name, NULL, NULL) == JVMTI_ERROR_NONE) {
+			if ((*jvmti)->GetMethodDeclaringClass(jvmti, method, &class) == JVMTI_ERROR_NONE) {		
+				if ((*jvmti)->GetClassSignature(jvmti, class, &class_signature, NULL) == JVMTI_ERROR_NONE) {
+					if (strcmp(method_name, "startFlag") == 0 && strcmp(class_signature, "Lorg/scalameter/Executor$Measurer$MethodCall;") == 0) {
+						flag = 1;
+						out = fopen("MethodCallDump", "w");
+					}
 
-	(*jvmti)->GetMethodName(jvmti, method, &method_name, NULL, NULL);
-	(*jvmti)->GetMethodDeclaringClass(jvmti, method, &class);
-	(*jvmti)->GetClassSignature(jvmti, class, &class_signature, NULL);
+					if (flag) {
+						fputs(class_signature, out);
+						fputs(method_name, out);
+						fputc('\n', out);
+					}
+					
+					if (strcmp(method_name, "endFlag") == 0 && strcmp(class_signature, "Lorg/scalameter/Executor$Measurer$MethodCall;") == 0) {
+						flag = 0;
+						fflush(out);
+						fclose(out);
+					}
 
-	if (strcmp(method_name, "startFlag") == 0 && strcmp(class_signature, "Lorg/scalameter/Executor$Measurer$MethodCall;") == 0) {
-		flag = 1;
-		out = fopen("MethodCallDump", "w");
-	}
-	if (flag) {
-		fputs(class_signature, out);
-		fputs(method_name, out);
-		fputc('\n', out);
-	}
-	if (strcmp(method_name, "endFlag") == 0 && strcmp(class_signature, "Lorg/scalameter/Executor$Measurer$MethodCall;") == 0) {
-		flag = 0;
-		fflush(out);
-		fclose(out);
-	}
+					(*jvmti)->Deallocate(jvmti, class_signature);
+				}
+			}
 
-	(*jvmti)->RawMonitorExit(jvmti, lock);
+			(*jvmti)->Deallocate(jvmti, method_name);
+		}
+
+		(*jvmti)->RawMonitorExit(jvmti, lock);
+	}
 }
 
 const jvmtiEventCallbacks callbacks = {
@@ -43,15 +51,37 @@ JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM *jvm, char *options, void *reserved) 
 	jvmtiEnv *jvmti = NULL;
 	jvmtiCapabilities capability;
 
-	(*jvm)->GetEnv(jvm, (void **) &jvmti, JVMTI_VERSION);
-	(*jvmti)->CreateRawMonitor(jvmti, "Method Call Monitor", &lock);
-	
-	(*jvmti)->GetCapabilities(jvmti, &capability);
-	capability.can_generate_method_entry_events = 1;
-	(*jvmti)->AddCapabilities(jvmti, &capability);
+	if ((*jvm)->GetEnv(jvm, (void **) &jvmti, JVMTI_VERSION) != JNI_OK) {
+		fprintf(stderr, "Fail to get JVMTI environment");
+		return JNI_ERR;
+	}
 
-	(*jvmti)->SetEventCallbacks(jvmti, &callbacks, sizeof(callbacks));
-	(*jvmti)->SetEventNotificationMode(jvmti, JVMTI_ENABLE, JVMTI_EVENT_METHOD_ENTRY, NULL);
+	if ((*jvmti)->CreateRawMonitor(jvmti, "Method Call Monitor", &lock) != JVMTI_ERROR_NONE) {
+		fprintf(stderr, "Fail to create raw monitor");
+		return JNI_ERR;
+	}
+	
+	if ((*jvmti)->GetCapabilities(jvmti, &capability) != JVMTI_ERROR_NONE) {
+		fprintf(stderr, "Fail to get capabilities");
+		return JNI_ERR;
+	}
+
+	capability.can_generate_method_entry_events = 1;
+	
+	if ((*jvmti)->AddCapabilities(jvmti, &capability) != JVMTI_ERROR_NONE) {
+		fprintf(stderr, "Fail to add capabilities");
+		return JNI_ERR;
+	}
+
+	if ((*jvmti)->SetEventCallbacks(jvmti, &callbacks, sizeof(callbacks)) != JVMTI_ERROR_NONE) {
+		fprintf(stderr, "Fail to set event callbacks");
+		return JNI_ERR;
+	}
+
+	if ((*jvmti)->SetEventNotificationMode(jvmti, JVMTI_ENABLE, JVMTI_EVENT_METHOD_ENTRY, NULL)) {
+		fprintf(stderr, "Fail to set event notification mode");
+		return JNI_ERR;
+	}
 
 	return JNI_OK;
 }
--- scalameter-tools/src/main/scala/org/scalameter/tools/package.scala
+++ scalameter-tools/src/main/scala/org/scalameter/tools/package.scala
@@ -3,7 +3,9 @@ package org.scalameter
 package object tools extends MeasureBuilder(Context.topLevel ++ Context(Key.exec.benchRuns -> 1), Executor.Warmer.NoWarmer(), null, null) {
   type Measurer = org.scalameter.Executor.Measurer
   type MemoryFootprint = org.scalameter.Executor.Measurer.MemoryFootprint
+  type MethodCall = org.scalamter.Executor.Measurer.MethodCall
   type DefaultWarmer = org.scalameter.Executor.Warmer.Default
   type NoWarmer = org.scalameter.Executor.Warmer.NoWarmer
   val exec = org.scalameter.Key.exec
+  val defaultContext = Context.topLevel ++ Context(Key.exec.benchRuns -> 1)
 }
