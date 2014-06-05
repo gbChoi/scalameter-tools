ac37aa842a0ac8b5d9077cfef9b36c402e182424
Gwangbae Choi <gwangbae.choi@epfl.ch>
2014-06-06 07:00:41 +0900

    org.scalameter was written as org.scalamter

--- scalameter-tools/src/main/scala/org/scalameter/tools/package.scala
+++ scalameter-tools/src/main/scala/org/scalameter/tools/package.scala
@@ -3,7 +3,7 @@ package org.scalameter
 package object tools extends MeasureBuilder(Context.topLevel ++ Context(Key.exec.benchRuns -> 1), Executor.Warmer.NoWarmer(), null, null, null, null) {
   type Measurer = org.scalameter.Executor.Measurer
   type MemoryFootprint = org.scalameter.Executor.Measurer.MemoryFootprint
-  type MethodCall = org.scalamter.Executor.Measurer.MethodCall
+  type MethodCall = org.scalameter.Executor.Measurer.MethodCall
   type DefaultWarmer = org.scalameter.Executor.Warmer.Default
   type NoWarmer = org.scalameter.Executor.Warmer.NoWarmer
   val exec = org.scalameter.Key.exec
