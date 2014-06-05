3473a0e0df4c4d7693bc4c21fef41564360ef25c
Gwangbae Choi <gwangbae.choi@epfl.ch>
2014-06-05 23:51:33 +0900

    mischosen argument in MeasureInstance is replaced

--- scalameter-tools/src/main/scala/org/scalameter/tools/MeasureInstance.scala
+++ scalameter-tools/src/main/scala/org/scalameter/tools/MeasureInstance.scala
@@ -5,14 +5,14 @@ import org.scalameter.Setup
 
 class MeasureInstance(setup:Setup[Unit], warmer:Executor.Warmer) {
   def apply(m: Measurer = new Executor.Measurer.Default)(snippet:  => Any): Double = {
-    for (i <- warmer.warming(setup.context, setup.setupBeforeAll, setup.teardownAfterAll))(snippet)
+    for (i <- warmer.warming(setup.context, setup.setupFor(Unit), setup.teardownFor(Unit)))(snippet)
     val result = m.measure[Unit, Unit](setup.context, setup.context.get(exec.benchRuns).get, setup.setupFor(), setup.teardownFor(), setup.regenerateFor(setup.gen.dataset.next), Unit => snippet)
     result.filter(_ >= 0).min
   }
   
   def bench(m: Measurer = new Executor.Measurer.Default)(afterMeasure: Double => Any)(snippet: => Any): Any = {
     val snippetResult = snippet
-    for (i <- warmer.warming(setup.context, setup.setupBeforeAll, setup.teardownAfterAll))(snippet)
+    for (i <- warmer.warming(setup.context, setup.setupFor(Unit), setup.teardownFor(Unit)))(snippet)
     val result = m.measure[Unit, Unit](setup.context, setup.context.get(exec.benchRuns).get, setup.setupFor(), setup.teardownFor(), setup.regenerateFor(setup.gen.dataset.next), Unit => snippet)
     afterMeasure(result.filter(_ >= 0).min)
     snippetResult
