5e0afc3b4659120945c756bec6ee18cc6a0386b3
Gwangbae Choi <gwangbae.choi@epfl.ch>
2014-06-06 07:16:44 +0900

    remove filter which remove negative results

--- scalameter-tools/src/main/scala/org/scalameter/tools/MeasureInstance.scala
+++ scalameter-tools/src/main/scala/org/scalameter/tools/MeasureInstance.scala
@@ -15,7 +15,7 @@ class MeasureInstance(setup:Setup[Unit], warmer:Executor.Warmer) {
     setup.setupBeforeAll()    
     val result = m.measure[Unit, Unit](setup.context, setup.context.get(exec.benchRuns).get, setup.setupFor(), setup.teardownFor(), setup.regenerateFor(setup.gen.dataset.next), Unit => snippet)
     setup.teardownAfterAll()
-    result.filter(_ >= 0).min
+    result.min
   }
   
   def bench(m: Measurer = new Executor.Measurer.Default)(afterMeasure: Double => Any)(snippet: => Any): Any = {
@@ -31,7 +31,7 @@ class MeasureInstance(setup:Setup[Unit], warmer:Executor.Warmer) {
     setup.setupBeforeAll()
     val result = m.measure[Unit, Unit](setup.context, setup.context.get(exec.benchRuns).get, setup.setupFor(), setup.teardownFor(), setup.regenerateFor(setup.gen.dataset.next), Unit => snippet)    
     setup.teardownAfterAll()
-    afterMeasure(result.filter(_ >= 0).min)
+    afterMeasure(result.min)
     snippetResult
   }
 }
